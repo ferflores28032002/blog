970c8fa361f635a352a75031329e370f0c44d84f
Fernando José Flores Mendoza <110640664+ferflores28032002@users.noreply.github.com>
2024-09-21 13:11:54 -0600

    se agregaron secciones

--- js/menu.js
+++ js/menu.js
@@ -2,16 +2,22 @@
     const openButton = document.querySelector('.nav__menu');
     const menu = document.querySelector('.nav__link');
     const closeMenu = document.querySelector('.nav__close');
+    const navLinks = document.querySelectorAll('.nav__links'); // Seleccionamos todos los enlaces del menú
 
-    openButton.addEventListener('click', ()=>{
+    // Abrir el menú
+    openButton.addEventListener('click', () => {
         menu.classList.add('nav__link--show');
     });
 
-    closeMenu.addEventListener('click', ()=>{
+    // Cerrar el menú cuando se haga clic en el botón de cerrar
+    closeMenu.addEventListener('click', () => {
         menu.classList.remove('nav__link--show');
     });
 
-    
-
-
+    // Cerrar el menú cuando se haga clic en cualquier enlace
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            menu.classList.remove('nav__link--show');
+        });
+    });
 })();
